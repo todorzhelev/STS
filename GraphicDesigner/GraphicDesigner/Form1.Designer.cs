bb90c16a068ef9e9272b3fa1d1ec311a251c07bc
Todor Zhelev <todorjelev36@gmail.com>
2015-03-10 19:59:08 +0200

    - update

--- GraphicDesigner/GraphicDesigner/Form1.Designer.cs
+++ GraphicDesigner/GraphicDesigner/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(789, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1192, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 602);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- GraphicDesigner/GraphicDesigner/Form1.cs
+++ GraphicDesigner/GraphicDesigner/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
+using System.Windows.Input;
+using System.Diagnostics;
 
 namespace GraphicDesigner
 {
@@ -15,6 +18,90 @@ namespace GraphicDesigner
         public Form1()
         {
             InitializeComponent();
+            graphics = this.CreateGraphics();
+            //graphics.Clear(Color.White);
+            this.MouseDown += mouseDown;
+            this.MouseUp += mouseUp;
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stopwatch t = new Stopwatch();
+            Graphics graphics = this.CreateGraphics();
+           // Rectangle rectangle = new Rectangle(50, 50, 150, 150);
+            //graphics.DrawEllipse(Pens.Black, rectangle);
+            //graphics.DrawRectangle(Pens.Red, rectangle);
+
+            //graphics.DrawLine(Pens.Black, 50, 50, 100,100);
+
+            //Point pt = new Point(5, 8);
+
+            //int x = Cursor.Position.X;
+            //int y = Cursor.Position.Y;
+
+            //Rectangle rectangle = new Rectangle(x - 100, y - 100, 1, 1);
+            //graphics.DrawRectangle(Pens.Black, rectangle);
+            
+           // Point[] coords = new Point[300];
+            var coords = new List<Point>();
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+
+            double radius = 20;
+          
+                radius =  150;
+                for (double i = 0; i < 360; i += 2)
+                {
+                    double angle = i * System.Math.PI / 180;
+                    int x = (int)(150 + radius * System.Math.Cos(angle));
+                    int y = (int)(150 + radius * System.Math.Sin(angle));
+                    Point p = new Point(x, y);
+                    coords.Add(p);
+                }
+
+                t.Start();
+                foreach (Point p in coords)
+                {
+                    graphics.FillRectangle(blueBrush, p.x, p.y, 2, 2);
+                    //Bitmap bm = new Bitmap(1, 1);
+                    //bm.SetPixel(0, 0, Color.Red);
+                    //graphics.DrawImageUnscaled(bm, p.x, p.y);
+                }
+                t.Stop();
+                TimeSpan ts =  t.Elapsed;
+        }
+
+        private void mouseDown(object sender, MouseEventArgs e)
+        {
+            this.x = e.X;
+            this.y = e.Y;
+
+           // graphics.DrawLine(Pens.Black, x, y, x+100, y+100);
+
+            //Rectangle rectangle = new Rectangle(x, y, 1, 1);
+            //graphics.DrawRectangle(Pens.Black, rectangle);
+        }
+
+        private void mouseUp(object sender, MouseEventArgs e)
+        {
+            //Bitmap bm = new Bitmap(1, 1);
+            //bm.SetPixel(0, 0, Color.Red);
+            //graphics.DrawImageUnscaled(bm, e.X, e.Y);
+
+           graphics.DrawLine(Pens.Black, x, y, e.X, e.Y);
+
+            //Rectangle rectangle = new Rectangle(x, y, 1, 1);
+            //graphics.DrawRectangle(Pens.Black, rectangle);
+           Point pt = new Point(3,4);
+           pt.mouseUp(sender, e);
+        }
+
+        private Graphics graphics;
+        private int x, y;
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
     }
 }
