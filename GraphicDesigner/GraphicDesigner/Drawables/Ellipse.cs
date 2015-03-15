4492fbe9765b8a7e7315b451b81329c8d914a285
Tsvetina Georgieva <tsv.georgieva@abv.bg>
2015-03-15 22:27:51 +0200

    added basic structure

---
+++ GraphicDesigner/GraphicDesigner/Drawables/Ellipse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicDesigner.Drawables
+{
+    class Ellipse : IDrawable
+    {
+        public IList<System.Drawing.PointF> GetPoints()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public FigureType FigureType
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ GraphicDesigner/GraphicDesigner/Utilities/InputOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GraphicDesigner
+{
+    class InputOptions
+    {
+        public Color Color { get; set; }
+        public FigureType FigureType { get; set; }
+        public int BrushSize { get; set; }
+    }
+}
