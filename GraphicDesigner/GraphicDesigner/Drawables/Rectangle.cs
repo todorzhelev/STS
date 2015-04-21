4f82bb502c546623980edc16ba85919aa120fedd
todorjelev36@gmail.com <todorjelev36@gmail.com>
2015-04-21 18:18:21 +0300

    + added basic rectangle logic

--- GraphicDesigner/GraphicDesigner/Drawables/Rectangle.cs
+++ GraphicDesigner/GraphicDesigner/Drawables/Rectangle.cs
@@ -11,22 +11,54 @@ namespace GraphicDesigner.Drawables
     {
         public IList<Point> GetPoints()
         {
-            throw new NotImplementedException();
+            var points = GeneratePoints();
+
+            return points;
         }
 
         public void mouseDown(Point mouseCoords)
         {
-            throw new NotImplementedException();
+            start = mouseCoords;
         }
 
+        public IList<Point> GeneratePoints()
+        {
+            var points = new List<Point>();
+
+            p1 = start;
+            p2 = end;
+            p3 = new Point(p1.X, p2.Y);
+            p4 = new Point(p2.X, p1.Y);
+
+            if(start.X == end.X || start.Y == end.Y)
+            {
+                return points;
+            }
+
+            AddLinePoints(p1, p4,  ref points);
+            AddLinePoints(p4, p2,  ref points);
+            AddLinePoints(p2, p3,  ref points);
+            AddLinePoints(p3, p1,  ref points);
+
+            return points;
+        }
+
+        public void AddLinePoints(Point p1, Point p2, ref List<Point> points)
+        {
+            Line l = new Line();
+            l.GeneratePoints(p1, p2);
+            for (int i = 0; i < l.GetPoints().Count; i++)
+            {
+                points.Add(l.GetPoints()[i]);
+            }
+        }
         public void mouseUp(Point mouseCoords)
         {
-            throw new NotImplementedException();
+            end = mouseCoords;
         }
 
         public void mouseMove(Point mouseCoords)
         {
-            throw new NotImplementedException();
         }
 
         public FigureType FigureType
@@ -40,5 +72,8 @@ namespace GraphicDesigner.Drawables
                 throw new NotImplementedException();
             }
         }
+
+        private Point start,end;
+        private Point p1, p2, p3, p4;
     }
 }
