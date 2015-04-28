6bbcea1570cdfb696c306f52ac3e49ba2113627b
Tsvetina Georgieva <tsv.georgieva@abv.bg>
2015-04-28 20:03:26 +0300

    added missing file

---
+++ GraphicDesigner/GraphicDesigner/Utilities/LayerLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicDesigner.Utilities
+{
+    public enum LayerLevel
+    {
+        Field = 0,
+        Last = 1,
+        Current = 2
+    }
+}
