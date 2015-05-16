878a4c09b75e4f71d3cf43ab1ae9430fb1ed0e15
todorjelev36@gmail.com <todorjelev36@gmail.com>
2015-05-16 10:47:38 +0300

    + added missing file

---
+++ GraphicDesigner/GraphicDesigner/Factories/ToolFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicDesigner
+{
+    static class ToolFactory
+    {
+        public static ITool GetTool(ToolType toolType)
+        {
+            switch (toolType)
+            {
+                case ToolType.Rotate:
+                    return new Tools.Rotate();
+                case ToolType.Select:
+                    return new Tools.Select();
+                case ToolType.Unknown:
+                    return new Tools.Select();
+                default:
+                    return new Tools.Select();
+            }
+        }
+    }
+}
